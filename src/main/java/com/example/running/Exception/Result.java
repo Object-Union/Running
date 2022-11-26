4844b0580eb86860529cc31eb7b0a1d89295cade
ObjectUnion <1398903944@qq.com>
2022-11-26 14:06:59 +0800

    11-26 提交

--- src/main/java/com/example/running/Exception/Result.java
+++ src/main/java/com/example/running/Exception/Result.java
@@ -1,8 +1,14 @@
 package com.example.running.Exception;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.stereotype.Component;
 
+@Data
+@ToString
+@Component
 @AllArgsConstructor
 @NoArgsConstructor
 public class Result {
